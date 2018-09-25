37b7c8de87434287e3405447a574cd2b1c361af6
Partho P. Das <parthopdas@live.com>
2018-09-26 08:50:31 +1000

    7. Listen to child component events

--- src/app/event/components/add-attendee/add-attendee.component.ts
+++ src/app/event/components/add-attendee/add-attendee.component.ts
@@ -1,24 +1,29 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-​
+import { Component, EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
 import { Attendee } from '../../../models';
-​
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-add-attendee',
   templateUrl: './add-attendee.component.html',
   styleUrls: ['./add-attendee.component.scss']
 })
 export class AddAttendeeComponent {
+  @Output()
+  addAttendee = new EventEmitter<Attendee>();
+
   addAttendeeForm = new FormGroup({
     name: new FormControl('', [Validators.required])
   });
-​
+
   submit() {
     const attendee = {
       name: this.addAttendeeForm.value.name,
       attending: true,
       guests: 0
     };
-    console.log('TCL: AddAttendeeComponent -> submit -> attendee', attendee);
+    this.addAttendee.emit(attendee);
   }
 }
