76f91e4dea22e31db38d509ee6c63deecd8abe36
Partho P. Das <parthopdas@live.com>
2018-09-26 08:51:40 +1000

    8. Add test for the event emitter

--- src/app/event/components/add-attendee/add-attendee.component.spec.ts
+++ src/app/event/components/add-attendee/add-attendee.component.spec.ts
@@ -33,4 +33,12 @@ describe('AddAttendeeComponent', () => {
     component.addAttendeeForm.controls.name.setValue('Duncan');
     expect(component.addAttendeeForm.valid).toEqual(true);
   });
+
+  it('should emit an attendee', async(() => {
+    component.addAttendeeForm.controls.name.setValue('Duncan');
+    component.addAttendee.subscribe((attendee: Attendee) => {
+      expect(attendee.name).toEqual('Duncan');
+    });
+    component.submit();
+  }));
 });
