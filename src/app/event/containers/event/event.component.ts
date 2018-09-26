5ddac6f792eba13bbc69f09616e6d284f9def77d
Partho P. Das <parthopdas@live.com>
2018-09-26 11:00:15 +1000

    20. Create effect

--- src/app/event/containers/event/event.component.ts
+++ src/app/event/containers/event/event.component.ts
@@ -4,9 +4,11 @@ import { Store, select } from '@ngrx/store';
 
 import { Attendee } from '../../../models';
 import { EventService } from '../../services/event.service';
-import { State } from '../../../state/state';
 import { StartSpinner, StopSpinner } from '../../../state/spinner/spinner.actions';
 import { getSpinner } from '../../../state/spinner/spinner.selectors';
+import { LoadAttendees } from '../../state/attendees/attendees.actions';
+import { State } from '../../state';
+import { getAttendees } from '../../state/attendees/attendees.selectors';
 
 @Component({
   selector: 'app-event',
@@ -23,19 +25,15 @@ export class EventComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.getAttendees();
     this.spinner$ = this.store.pipe(select(getSpinner));
-  }
-
-  getAttendees() {
-    this.attendees$ = this.eventService.getAttendees();
+    this.attendees$ = this.store.pipe(select(getAttendees));
+    this.store.dispatch(new LoadAttendees());
   }
 
   addAttendee(attendee: Attendee) {
     this.store.dispatch(new StartSpinner());
     this.eventService.addAttendee(attendee).subscribe(() => {
       this.store.dispatch(new StopSpinner());
-      this.getAttendees();
     });
   }
 }
