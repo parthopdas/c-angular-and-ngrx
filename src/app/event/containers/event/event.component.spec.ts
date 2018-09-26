cea58ccf35ea8ac5142fa0a05aa4b26a56657f78
Partho P. Das <parthopdas@live.com>
2018-09-26 11:11:25 +1000

    25. Fix EventComponent tests

--- src/app/event/containers/event/event.component.spec.ts
+++ src/app/event/containers/event/event.component.spec.ts
@@ -1,28 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of, BehaviorSubject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { EventComponent } from './event.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { EventService } from '../../services/event.service';
-import { Store } from '@ngrx/store';
+import { State } from '../../state';
+import { Attendee } from '../../../models';
 
 describe('EventComponent', () => {
   let component: EventComponent;
   let fixture: ComponentFixture<EventComponent>;
+  let service: EventService;
+  let store: Store<State>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       providers: [
+        { provide: HttpClient, useValue: null },
         {
           provide: EventService,
           useValue: {
-            getAttendees: () => {},
+            getAttendees: () => {}
           }
         },
         {
           provide: Store,
           useValue: {
             pipe: () => {},
-            dispatch: () => {}
+            dispatch: jest.fn()
           }
         }
       ],
@@ -34,10 +44,29 @@ describe('EventComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EventComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(EventService);
+    store = TestBed.get(Store);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a list of attendees set', () => {
+
+    const subject = new BehaviorSubject<Attendee[]>(null);
+
+    const fakeAttendees = [{ name: 'FAKE_NAME', attending: false, guests: 0 }];
+
+    jest.spyOn(store, 'pipe').mockImplementation(() => subject);
+
+    subject.next(fakeAttendees);
+
+    component.ngOnInit();
+
+    component.attendees$.subscribe(attendees => {
+      expect(attendees).toEqual(fakeAttendees);
+    });
   });
 });
